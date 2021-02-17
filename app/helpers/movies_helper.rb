03a463338af87e5c05d7904c39454c9104d16ce8
Alex Ines <alex.ines@pm.me>
2021-02-17 14:06:23 +0100

    Add images and stylesheets

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,7 @@
 module MoviesHelper
+
+  def display_total_gross(gross)
+    gross.flop? ? "Flop!" : number_to_currency(gross, )
+    
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+
+  def flop?
+    total_gross < 225000000000
+  end
 end
